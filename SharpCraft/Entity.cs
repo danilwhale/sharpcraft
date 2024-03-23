d8790d85b4ec7c3868d79aaa81d74fa143835c05
danil <onefiler1@hotmail.com>
2024-03-23 21:52:43 +0200

    [core] move some player code to entity class

---
+++ SharpCraft/Entity.cs
@@ -0,0 +1,105 @@
+﻿using System.Numerics;
+using SharpCraft.Physics;
+
+namespace SharpCraft;
+
+public class Entity(Level.Level level, float halfWidth, float halfHeight)
+{
+    protected Vector3 LastPosition;
+    public Vector3 Position;
+    protected Vector3 Direction;
+    protected BoundingBox BBox;
+    
+    protected bool IsOnGround;
+    
+    protected float Yaw;
+    protected float Pitch;
+    
+    protected void MoveToRandom()
+    {
+        var x = Random.Shared.NextSingle() * level.Width;
+        var y = level.Height + 10;
+        var z = Random.Shared.NextSingle() * level.Length;
+        MoveTo(new Vector3(x, y, z));
+    }
+
+    protected void MoveTo(Vector3 newPosition)
+    {
+        Position = newPosition;
+        BBox = new BoundingBox(
+            newPosition - new Vector3(halfWidth, halfHeight, halfWidth),
+            newPosition + new Vector3(halfWidth, halfHeight, halfWidth)
+        );
+    }
+
+    public virtual void Rotate(float pitch, float yaw)
+    {
+        Pitch += pitch;
+        Yaw -= yaw;
+    }
+
+    public virtual void Tick()
+    {
+        LastPosition = Position;
+    }
+    
+    protected void Move(Vector3 direction)
+    {
+        var oldDirection = direction;
+
+        var boxes = level.GetBoxes(BBox.Expand(direction));
+        
+        foreach (var box in boxes)
+        {
+            direction.X = box.ClipXCollide(BBox, direction.X);
+        }
+
+        BBox.Move(direction.X, 0.0f, 0.0f);
+        
+        foreach (var box in boxes)
+        {
+            direction.Y = box.ClipYCollide(BBox, direction.Y);
+        }
+
+        BBox.Move(0.0f, direction.Y, 0.0f);
+        
+        foreach (var box in boxes)
+        {
+            direction.Z = box.ClipZCollide(BBox, direction.Z);
+        }
+
+        BBox.Move(0.0f, 0.0f, direction.Z);
+
+        // ReSharper disable CompareOfFloatsByEqualityOperator
+        IsOnGround = oldDirection.Y != direction.Y && oldDirection.Y < 0.0f;
+
+        Direction.X = oldDirection.X != direction.X ? 0.0f : direction.X;
+        Direction.Y = oldDirection.Y != direction.Y ? 0.0f : direction.Y;
+        Direction.Z = oldDirection.Z != direction.Z ? 0.0f : direction.Z;
+        // ReSharper restore CompareOfFloatsByEqualityOperator
+
+        Position = new Vector3(
+            (BBox.Min.X + BBox.Max.X) / 2.0f,
+            BBox.Min.Y + 1.62f,
+            (BBox.Min.Z + BBox.Max.Z) / 2.0f
+        );
+    }
+
+    protected void MoveRelative(float x, float z, float speed)
+    {
+        var dist = x * x + z * z;
+
+        if (dist < 0.01f) return;
+
+        dist = speed / MathF.Sqrt(dist);
+
+        x *= dist;
+        z *= dist;
+
+        var sin = MathF.Sin(Yaw * DEG2RAD);
+        var cos = MathF.Cos(Yaw * DEG2RAD);
+
+        Direction.X += x * cos + z * sin;
+        Direction.Z += z * cos - x * sin;
+    }
+}
--- SharpCraft/Player.cs
+++ SharpCraft/Player.cs
@@ -3,64 +3,27 @@ using SharpCraft.Physics;
 
 namespace SharpCraft;
 
-public class Player
+public class Player : Entity
 {
     private const float MouseSensitivity = 0.075f;
     private const float HalfWidth = 0.3f;
     private const float HalfHeight = 0.9f;
-
-    private Vector3 _lastPosition;
-    public Vector3 Position;
-    private Vector3 _direction;
-    private BoundingBox _bbox;
-
+    
     public Camera3D Camera;
 
-    private float _yaw;
-    private float _pitch;
-    private bool _isOnGround;
-
-    private readonly Level.Level _level;
-
-    public Player(Level.Level level)
+    public Player(Level.Level level) : base(level, HalfWidth, HalfHeight)
     {
-        _level = level;
         Camera = new Camera3D(Vector3.Zero, Vector3.Zero, Vector3.UnitY, 70.0f, CameraProjection.Perspective);
         MoveToRandom();
     }
-
-    private void MoveToRandom()
-    {
-        var x = Random.Shared.NextSingle() * _level.Width;
-        var y = _level.Height + 10;
-        var z = Random.Shared.NextSingle() * _level.Length;
-        MoveTo(new Vector3(x, y, z));
-    }
-
-    private void MoveTo(Vector3 newPosition)
-    {
-        Position = newPosition;
-        _bbox = new BoundingBox(
-            newPosition - new Vector3(HalfWidth, HalfHeight, HalfWidth),
-            newPosition + new Vector3(HalfWidth, HalfHeight, HalfWidth)
-        );
-    }
-
-    public void Rotate(float pitch, float yaw)
-    {
-        _pitch += pitch * MouseSensitivity;
-        _pitch = Math.Clamp(_pitch, -89.0f, 89.0f);
-        
-        _yaw -= yaw * MouseSensitivity;
-    }
-
+    
     public void MoveCamera(float lastDelta)
     {
-        Camera.Position = _lastPosition + (Position - _lastPosition) * lastDelta;
+        Camera.Position = LastPosition + (Position - LastPosition) * lastDelta;
 
         var rotation = Matrix4x4.CreateFromYawPitchRoll(
-            _yaw * DEG2RAD,
-            _pitch * DEG2RAD,
+            Yaw * DEG2RAD,
+            Pitch * DEG2RAD,
             0.0f
         );
 
@@ -69,9 +32,15 @@ public class Player
         Camera.Target = Camera.Position + forward;
     }
 
-    public void Tick()
+    public override void Rotate(float pitch, float yaw)
     {
-        _lastPosition = Position;
+        base.Rotate(pitch * MouseSensitivity, yaw * MouseSensitivity);
+        Pitch = Math.Clamp(Pitch, -89.0f, 89.0f);
+    }
+
+    public override void Tick()
+    {
+        base.Tick();
         
         if (IsKeyDown(KeyboardKey.R)) MoveToRandom();
 
@@ -83,77 +52,17 @@ public class Player
             : IsKeyDown(KeyboardKey.S) || IsKeyDown(KeyboardKey.Down) ? -1
             : 0;
 
-        if (_isOnGround && (IsKeyDown(KeyboardKey.Space) || IsKeyDown(KeyboardKey.LeftSuper)))
+        if (IsOnGround && (IsKeyDown(KeyboardKey.Space) || IsKeyDown(KeyboardKey.LeftSuper)))
         {
-            _direction.Y = 0.12f;
+            Direction.Y = 0.12f;
         }
 
-        MoveRelative(x, z, _isOnGround ? 0.02f : 0.005f);
-        _direction.Y -= 0.005f;
-        Move(_direction);
+        MoveRelative(x, z, IsOnGround ? 0.02f : 0.005f);
+        Direction.Y -= 0.005f;
+        Move(Direction);
         
-        _direction *= new Vector3(0.91f, 0.98f, 0.91f);
+        Direction *= new Vector3(0.91f, 0.98f, 0.91f);
 
-        if (_isOnGround) _direction *= new Vector3(0.8f, 1.0f, 0.8f);
-    }
-
-    private void Move(Vector3 direction)
-    {
-        var oldDirection = direction;
-
-        var boxes = _level.GetBoxes(_bbox.Expand(direction));
-        
-        foreach (var box in boxes)
-        {
-            direction.X = box.ClipXCollide(_bbox, direction.X);
-        }
-
-        _bbox.Move(direction.X, 0.0f, 0.0f);
-        
-        foreach (var box in boxes)
-        {
-            direction.Y = box.ClipYCollide(_bbox, direction.Y);
-        }
-
-        _bbox.Move(0.0f, direction.Y, 0.0f);
-        
-        foreach (var box in boxes)
-        {
-            direction.Z = box.ClipZCollide(_bbox, direction.Z);
-        }
-
-        _bbox.Move(0.0f, 0.0f, direction.Z);
-
-        // ReSharper disable CompareOfFloatsByEqualityOperator
-        _isOnGround = oldDirection.Y != direction.Y && oldDirection.Y < 0.0f;
-
-        _direction.X = oldDirection.X != direction.X ? 0.0f : direction.X;
-        _direction.Y = oldDirection.Y != direction.Y ? 0.0f : direction.Y;
-        _direction.Z = oldDirection.Z != direction.Z ? 0.0f : direction.Z;
-        // ReSharper restore CompareOfFloatsByEqualityOperator
-
-        Position = new Vector3(
-            (_bbox.Min.X + _bbox.Max.X) / 2.0f,
-            _bbox.Min.Y + 1.62f,
-            (_bbox.Min.Z + _bbox.Max.Z) / 2.0f
-        );
-    }
-
-    private void MoveRelative(float x, float z, float speed)
-    {
-        var dist = x * x + z * z;
-
-        if (dist < 0.01f) return;
-
-        dist = speed / MathF.Sqrt(dist);
-
-        x *= dist;
-        z *= dist;
-
-        var sin = MathF.Sin(_yaw * DEG2RAD);
-        var cos = MathF.Cos(_yaw * DEG2RAD);
-
-        _direction.X += x * cos + z * sin;
-        _direction.Z += z * cos - x * sin;
+        if (IsOnGround) Direction *= new Vector3(0.8f, 1.0f, 0.8f);
     }
 }
