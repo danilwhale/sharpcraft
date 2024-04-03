e7b6987db2beed6fce444c06a21a9298f288eaee
danil <onefiler1@hotmail.com>
2024-04-03 18:51:26 +0300

    [gameplay] revamp splash screen

--- SharpCraft/Scenes/SplashScene.cs
+++ SharpCraft/Scenes/SplashScene.cs
@@ -6,6 +6,7 @@ namespace SharpCraft.Scenes;
 public class SplashScene : IScene
 {
     private readonly double _startTime = GetTime();
+    private readonly int _seed = Random.Shared.Next();
 
     public void Update()
     {
@@ -17,17 +18,28 @@ public class SplashScene : IScene
 
     public void Draw()
     {
-        ClearBackground(Color.SkyBlue);
+        ClearBackground(ColorBrightness(Color.SkyBlue, -0.6f));
 
         var terrain = ResourceManager.GetTexture("terrain.png");
 
+        var random = new Random(_seed);
+        
+        var halfHeight = GetScreenHeight() / 128 / 2;
+
         for (var x = 0; x < GetScreenWidth() / 128; x++)
         {
+            var level = halfHeight + random.Next(1, 3);
+            
             for (var y = 0; y < GetScreenHeight() / 128; y++)
             {
+                Rectangle source;
+                if (y == halfHeight || y == level) source = new Rectangle(0, 0, 16, 16);
+                else if (y < halfHeight) continue;
+                else source = new Rectangle(16, 0, 16, 16);
+
                 DrawTexturePro(
                     terrain,
-                    new Rectangle(16, 0, 16, 16),
+                    source,
                     new Rectangle(x * 128, y * 128, 128, 128),
                     Vector2.Zero,
                     0.0f,
@@ -36,7 +48,10 @@ public class SplashScene : IScene
             }
         }
         
+        DrawRectangleGradientV(0, 0, GetScreenWidth(), GetScreenHeight(), Color.Blank, ColorAlpha(Color.Black, 0.6f));
+        
         DrawTextCentered("sharpcraft", 48, 0, Color.White);
+        DrawTextCentered("comically long splash screen for fun", 16, GetScreenHeight() / 2 - 48, Color.White);
     }
 
     private void DrawTextCentered(string text, int fontSize, int yOffset, Color color)
