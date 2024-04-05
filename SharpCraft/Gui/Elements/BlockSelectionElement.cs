abcf1619e6b8eefa113745db4b58cb49994c4106
danil <61111955+danilwhale@users.noreply.github.com>
2024-04-05 22:27:10 +0300

    [gameplay] make block selection better

--- SharpCraft/Gui/Elements/BlockSelectionElement.cs
+++ SharpCraft/Gui/Elements/BlockSelectionElement.cs
@@ -6,7 +6,10 @@ namespace SharpCraft.Gui.Elements;
 
 public class BlockSelectionElement : Element
 {
+    private const int Size = 48;
+    
     public byte CurrentTile = 1;
+    public Range PreviewRange = 1..TileRegistry.GetNonNullTileCount();
     
     public override void Update()
     {
@@ -15,14 +18,33 @@ public class BlockSelectionElement : Element
 
     public override void Draw()
     {
-        DrawRectangle(16 - 4, GetScreenHeight() - 96 - 16 - 4, 96 + 8, 96 + 8, Color.White);
-
-        var textureIndex = TileRegistry.Tiles[CurrentTile].TextureIndex;
+        var textureIndex = TileRegistry.Tiles[CurrentTile]?.TextureIndex ?? 1;
         
+        DrawTile(16, GetScreenHeight() - 112, 96, 4, textureIndex);
+
+        for (var i = PreviewRange.Start.Value; i <= PreviewRange.End.Value; i++)
+        {
+            var tile = TileRegistry.Tiles[i]!;
+
+            var x = 64 + i * (Size + 8);
+            var y = GetScreenHeight() - 64;
+            
+            DrawTile(x, y, Size, 4, tile.TextureIndex);
+
+            if (CurrentTile == tile.Id)
+            {
+                DrawRectangle(x, y + Size - 4, Size, 4, Color.SkyBlue);
+            }
+        }
+    }
+
+    private void DrawTile(int x, int y, int size, int padding, int textureIndex)
+    {
+        DrawRectangle(x, y, size, size, Color.White);
         DrawTexturePro(
             ResourceManager.GetTexture("terrain.png"),
             new Rectangle(textureIndex % 16 * 16, textureIndex / 16 * 16, 16, 16),
-            new Rectangle(16, GetScreenHeight() - 96 - 16, 96, 96),
+            new Rectangle(x + padding, y + padding, size - padding * 2, size - padding * 2),
             Vector2.Zero,
             0.0f,
             Color.White);
