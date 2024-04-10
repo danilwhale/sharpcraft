5eb87e2cd783bf1400f64cff7e68cb814b728194
danil <61111955+danilwhale@users.noreply.github.com>
2024-04-10 17:58:15 +0300

    [gameplay] make leaves transparent

--- SharpCraft/Level/Tiles/TileRegistry.cs
+++ SharpCraft/Level/Tiles/TileRegistry.cs
@@ -13,7 +13,7 @@ public static class TileRegistry
     public static readonly Tile Sand = new(5, 5);
     public static readonly Tile Rock = new(6, 1);
     public static readonly Tile Wood = new WoodTile(7);
-    public static readonly Tile Leaves = new(8, 9);
+    public static readonly Tile Leaves = new LeavesTile(8, 9);
     public static readonly Tile WoodenPole = new PoleTile(9, 10, 4);
     public static readonly Tile StonePole = new PoleTile(10, 11, 1);
     
---
+++ SharpCraft/Level/Tiles/Types/LeavesTile.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpCraft.Level.Tiles.Types;
+
+public class LeavesTile(byte id, int textureIndex) : 
+    Tile(id, textureIndex, new TileConfig(IsLightBlocker: false, Layer: TileLayer.Translucent))
+{
+    protected override bool ShouldKeepFace(Level level, int x, int y, int z, Face face)
+    {
+        return true;
+    }
+}
