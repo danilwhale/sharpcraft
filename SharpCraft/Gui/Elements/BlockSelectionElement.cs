ee81b265305c4b2daafbe0329c3f4b6427e05097
danil <61111955+danilwhale@users.noreply.github.com>
2024-06-22 17:06:42 +0300

    [gui] update hotbar

--- SharpCraft/Gui/Elements/BlockSelectionElement.cs
+++ SharpCraft/Gui/Elements/BlockSelectionElement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Numerics;
 using SharpCraft.Level.Blocks;
+using SharpCraft.Rendering;
 using SharpCraft.Utilities;
 
 namespace SharpCraft.Gui.Elements;
@@ -7,20 +8,19 @@ namespace SharpCraft.Gui.Elements;
 public class BlockSelectionElement : Element
 {
     private const int Size = 48;
-    
+
     public byte CurrentBlock = 1;
     public Range PreviewRange = 1..BlockRegistry.GetExistingBlockCount();
-    
+
     public override void Update()
     {
-        
     }
 
     public override void Draw()
     {
-        var textureIndex = BlockRegistry.Blocks[CurrentBlock]?.TextureIndex ?? 1;
-        
-        DrawBlockFace(16, GetScreenHeight() - 112, 96, 4, textureIndex);
+        var currentBlock = BlockRegistry.Blocks[CurrentBlock];
+
+        DrawBlock(16, GetScreenHeight() - 112, 96, currentBlock!);
 
         for (var i = PreviewRange.Start.Value; i <= PreviewRange.End.Value; i++)
         {
@@ -28,25 +28,44 @@ public class BlockSelectionElement : Element
 
             var x = 64 + i * (Size + 8);
             var y = GetScreenHeight() - 64;
-            
-            DrawBlockFace(x, y, Size, 4, block.TextureIndex);
+
+            DrawBlock(x, y, Size, block);
 
             if (CurrentBlock == block.Id)
             {
-                DrawRectangle(x, y + Size - 4, Size, 4, Color.SkyBlue);
+                DrawRectangleLinesEx(new Rectangle(x - 4.0f, y - 4.0f, Size + 8.0f, Size + 8.0f), 4.0f, Color.SkyBlue);
             }
         }
     }
 
-    private void DrawBlockFace(int x, int y, int size, int padding, int textureIndex)
+    private void DrawBlock(int x, int y, int size, Block block)
     {
         DrawRectangle(x, y, size, size, Color.White);
-        DrawTexturePro(
-            ResourceManager.GetTexture("terrain.png"),
-            new Rectangle(textureIndex % 16 * 16, textureIndex / 16 * 16, 16, 16),
-            new Rectangle(x + padding, y + padding, size - padding * 2, size - padding * 2),
-            Vector2.Zero,
-            0.0f,
-            Color.White);
+        
+        Rlgl.MatrixMode(MatrixMode.Projection);
+        Rlgl.LoadIdentity();
+        Rlgl.Ortho(0.0, GetScreenWidth(), GetScreenHeight(), 0.0, 0.01, 1000.0);
+        
+        Rlgl.MatrixMode(MatrixMode.ModelView);
+        
+        Rlgl.PushMatrix();
+        Rlgl.LoadIdentity();
+        
+        Rlgl.Translatef(x + size * 0.6f, y + size * 0.9f, -200.0f);
+        Rlgl.Scalef(size * 0.5f, size * 0.5f, size * 0.5f);
+        Rlgl.Rotatef(30.0f, 1.0f, 0.0f, 0.0f);
+        Rlgl.Rotatef(30.0f, 0.0f, 1.0f, 0.0f);
+        
+        Rlgl.Scalef(-1.0f, -1.0f, 1.0f);
+        
+        Rlgl.Begin(DrawMode.Triangles);
+        Rlgl.SetTexture(ResourceManager.GetTexture("terrain.png").Id);
+        
+        block.Build(RlglBuilder.Instance, 0, 0, 0);
+        
+        Rlgl.SetTexture(0);
+        Rlgl.End();
+        
+        Rlgl.PopMatrix();
     }
 }
