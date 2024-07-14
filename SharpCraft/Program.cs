8837f379b47f0d030902a839e9d09886e7faace8
danil <61111955+danilwhale@users.noreply.github.com>
2024-07-14 15:45:13 +0300

    implement extracting assets from jar

--- SharpCraft/Program.cs
+++ SharpCraft/Program.cs
@@ -3,6 +3,7 @@ global using Raylib_cs;
 using System.Runtime.CompilerServices;
 using SharpCraft.Scenes;
 using SharpCraft.Utilities;
+using TinyDialogsNet;
 
 [assembly: DisableRuntimeMarshalling]
 
@@ -10,38 +11,78 @@ namespace SharpCraft;
 
 internal static class Program
 {
+    private const string Version = "rd-161348";
+
     public static IScene Scene = null!;
-
-    private static readonly string[] RequiredFiles = ["terrain.png", "char.png"]; 
-
+    
     private static void Main()
     {
         GpuUtil.TryForceNvidiaGpu();
-        
+
         InitWindow(1024, 768, "SharpCraft");
         SetTraceLogLevel(TraceLogLevel.Warning);
 
-        if (!RequiredFiles.All(file => File.Exists(Path.Join(Assets.Root, file))))
+        if (!TryPrepareAssets())
         {
-            Scene = new NoAssetsScene();
+            CloseWindow();
+            return;
         }
-        else Scene = new GameScene();
+
+        Scene = new GameScene();
 
         while (!WindowShouldClose())
         {
             Scene.Update();
-            
+
             BeginDrawing();
-            
+
             Scene.Draw();
-            
+
             EndDrawing();
         }
 
         Scene.Dispose();
-        
+
         Assets.Unload();
 
         CloseWindow();
+    }
+
+    private static bool TryPrepareAssets()
+    {
+        var lastVersionPath = Path.Join(Assets.Root, ".last_version");
+        
+        if (AssetsExtractor.AreFilesPresent() &&
+            File.Exists(lastVersionPath) && File.ReadAllText(lastVersionPath) == Version) return true;
+        
+        try
+        {
+            var result = TinyDialogs.OpenFileDialog(
+                $"Select {Version} .jar file",
+                filter: new FileFilter(".jar files", ["*.jar"])
+            );
+
+            if (result.Canceled)
+            {
+                return false;
+            }
+
+            AssetsExtractor.Extract(result.Paths.First());
+            File.WriteAllText(lastVersionPath, Version);
+        }
+        catch (Exception e)
+        {
+            TinyDialogs.MessageBox(
+                "Failed to prepare assets", 
+                e.ToString(), 
+                MessageBoxDialogType.Ok, 
+                MessageBoxIconType.Error, 
+                MessageBoxButton.Ok
+            );
+            
+            return false;
+        }
+
+        return true;
     }
 }
