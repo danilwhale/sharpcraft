9891890ff9036a9fe8df8b5225bc2e3fc907b331
danilwhale <61111955+danilwhale@users.noreply.github.com>
2024-08-13 17:16:59 +0300

    generate icon for the window

--- SharpCraft/Program.cs
+++ SharpCraft/Program.cs
@@ -5,8 +5,10 @@ using System.Runtime.InteropServices;
 using Serilog;
 using Serilog.Events;
 using SharpCraft.Gui;
+using SharpCraft.Rendering;
 using SharpCraft.Scenes;
 using SharpCraft.Utilities;
+using SharpCraft.World.Rendering;
 using TinyDialogsNet;
 
 [assembly: DisableRuntimeMarshalling]
@@ -27,6 +29,7 @@ internal static class Program
         
         InitWindow(854, 480, "SharpCraft " + Version);
         SetWindowState(ConfigFlags.ResizableWindow);
+        GenerateWindowIcon();
         SetTraceLogLevel(TraceLogLevel.Warning);
         SetExitKey(KeyboardKey.Null);
 
@@ -79,6 +82,50 @@ internal static class Program
             TraceLogLevel.Fatal => LogEventLevel.Fatal,
             _ => LogEventLevel.Verbose
         }, Logging.GetLogMessage((nint)format, (nint)args));
+    }
+
+    private static void GenerateWindowIcon()
+    {
+        var target = LoadRenderTexture(64, 64);
+        
+        BeginDrawing();
+        BeginTextureMode(target);
+        
+        ClearBackground(Color.Blank);
+        
+        Rlgl.MatrixMode(MatrixMode.Projection);
+        Rlgl.LoadIdentity();
+        Rlgl.Ortho(0.0, target.Texture.Width, target.Texture.Height, 0.0, 0.01, 1000.0);
+        
+        Rlgl.MatrixMode(MatrixMode.ModelView);
+        Rlgl.LoadIdentity();
+        Rlgl.Translatef(0.0f, 0.0f, -200.0f);
+        
+        Rlgl.Scalef(40.0f, 40.0f, 40.0f);
+
+        Rlgl.Rotatef(30.0f, 1.0f, 0.0f, 0.0f);
+        Rlgl.Rotatef(45.0f, 0.0f, 1.0f, 0.0f);
+
+        Rlgl.Translatef(2.6f, -0.85f, -1.5f);
+        
+        Rlgl.Scalef(-1.0f, 1.0f, 1.0f);
+        
+        Rlgl.Begin(DrawMode.Quads);
+        Rlgl.SetTexture(Assets.GetTexture("IconResources.png").Id);
+        
+        Registries.Tiles.Grass.Build(RlglVertexBuilder.Instance, null, 0, 0, 0, RenderLayer.Lit);
+
+        Rlgl.SetTexture(Rlgl.GetTextureIdDefault());
+        Rlgl.End();
+        
+        EndTextureMode();
+        EndDrawing();
+
+        var image = LoadImageFromTexture(target.Texture);
+        UnloadRenderTexture(target);
+
+        SetWindowIcon(image);
+        UnloadImage(image);
     }
 
     private static bool TryPrepareAssets()
